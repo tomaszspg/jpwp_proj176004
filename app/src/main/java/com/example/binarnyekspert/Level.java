b096ef71159dbbce373b422b1d1919ae964be072
tomaszspg <s176004@student.pg.edu.pl>
2020-12-28 22:34:37 +0100

    28gru

--- app/src/main/java/com/example/binarnyekspert/Level.java
+++ app/src/main/java/com/example/binarnyekspert/Level.java
@@ -19,13 +19,11 @@ import java.util.concurrent.TimeUnit;
 
 public class Level extends AppCompatActivity implements View.OnClickListener {
     private Switch switch1;
-    private TextView pod16;
-    private Button bt1_1;
-    private Button bt1_2;
-    private Button bt1_3;
-    private Button bt1_4;
-    private Button bt1_5;
-    private Button bt1_6;
+    private Button bt1_1, bt1_2, bt1_3, bt1_4, bt1_5, bt1_6, bt2_1, bt2_2, bt2_3, bt2_4, bt2_5, bt2_6;
+    private Button bt3_1, bt3_2, bt3_3, bt3_4, bt3_5, bt3_6, bt4_1, bt4_2, bt4_3, bt4_4, bt4_5, bt4_6;
+    private Button bt5_1, bt5_2, bt5_3, bt5_4, bt5_5, bt5_6;
+
+
 //    private List<Button> buttons;
 //    private static final int[] BUTTON_IDS = {
 //            R.id.btn1_1,
@@ -44,27 +42,56 @@ public class Level extends AppCompatActivity implements View.OnClickListener {
         switch1 = (Switch) findViewById(R.id.switch1);
 
 
+
+
         TextView pod16 = findViewById(R.id.pod16);
+        TextView podramka = findViewById(R.id.podramka);
         TextView pod8 = (TextView) findViewById(R.id.pod8);
         TextView pod4 = (TextView) findViewById(R.id.pod4);
         TextView pod2 = (TextView) findViewById(R.id.pod2);
         TextView pod1 = (TextView) findViewById(R.id.pod1);
-        bt1_1 = (Button) findViewById(R.id.bt1_1);
-        bt1_1.setOnClickListener(this);
-        bt1_2 = (Button) findViewById(R.id.bt1_2);
-        bt1_2.setOnClickListener(this);
-        bt1_3 = (Button) findViewById(R.id.bt1_3);
-        bt1_3.setOnClickListener(this);
-        bt1_4 = (Button) findViewById(R.id.bt1_4);
-        bt1_4.setOnClickListener(this);
-        bt1_5 = (Button) findViewById(R.id.bt1_5);
-        bt1_5.setOnClickListener(this);
-        bt1_6 = (Button) findViewById(R.id.bt1_6);
-        bt1_6.setOnClickListener(this);
+        bt1_1 = (Button) findViewById(R.id.bt1_1); bt1_1.setOnClickListener(this);
+        bt1_2 = (Button) findViewById(R.id.bt1_2); bt1_2.setOnClickListener(this);
+        bt1_3 = (Button) findViewById(R.id.bt1_3); bt1_3.setOnClickListener(this);
+        bt1_4 = (Button) findViewById(R.id.bt1_4); bt1_4.setOnClickListener(this);
+        bt1_5 = (Button) findViewById(R.id.bt1_5); bt1_5.setOnClickListener(this);
+        bt1_6 = (Button) findViewById(R.id.bt1_6); bt1_6.setOnClickListener(this);
+        bt2_1 = (Button) findViewById(R.id.bt2_1); bt2_1.setOnClickListener(this);
+        bt2_2 = (Button) findViewById(R.id.bt2_2); bt2_2.setOnClickListener(this);
+        bt2_3 = (Button) findViewById(R.id.bt2_3); bt2_3.setOnClickListener(this);
+        bt2_4 = (Button) findViewById(R.id.bt2_4); bt2_4.setOnClickListener(this);
+        bt2_5 = (Button) findViewById(R.id.bt2_5); bt2_5.setOnClickListener(this);
+        bt2_6 = (Button) findViewById(R.id.bt2_6); bt2_6.setOnClickListener(this);
+        bt3_1 = (Button) findViewById(R.id.bt3_1); bt3_1.setOnClickListener(this);
+        bt3_2 = (Button) findViewById(R.id.bt3_2); bt3_2.setOnClickListener(this);
+        bt3_3 = (Button) findViewById(R.id.bt3_3); bt3_3.setOnClickListener(this);
+        bt3_4 = (Button) findViewById(R.id.bt3_4); bt3_4.setOnClickListener(this);
+        bt3_5 = (Button) findViewById(R.id.bt3_5); bt3_5.setOnClickListener(this);
+        bt3_6 = (Button) findViewById(R.id.bt3_6); bt3_6.setOnClickListener(this);
+        bt4_1 = (Button) findViewById(R.id.bt4_1); bt4_1.setOnClickListener(this);
+        bt4_2 = (Button) findViewById(R.id.bt4_2); bt4_2.setOnClickListener(this);
+        bt4_3 = (Button) findViewById(R.id.bt4_3); bt4_3.setOnClickListener(this);
+        bt4_4 = (Button) findViewById(R.id.bt4_4); bt4_4.setOnClickListener(this);
+        bt4_5 = (Button) findViewById(R.id.bt4_5); bt4_5.setOnClickListener(this);
+        bt4_6 = (Button) findViewById(R.id.bt4_6); bt4_6.setOnClickListener(this);
+        bt5_1 = (Button) findViewById(R.id.bt5_1); bt5_1.setOnClickListener(this);
+        bt5_2 = (Button) findViewById(R.id.bt5_2); bt5_2.setOnClickListener(this);
+        bt5_3 = (Button) findViewById(R.id.bt5_3); bt5_3.setOnClickListener(this);
+        bt5_4 = (Button) findViewById(R.id.bt5_4); bt5_4.setOnClickListener(this);
+        bt5_5 = (Button) findViewById(R.id.bt5_5); bt5_5.setOnClickListener(this);
+        bt5_6 = (Button) findViewById(R.id.bt5_6); bt5_6.setOnClickListener(this);
+        Random rand = new Random();
+        bt1_6.setText(Integer.toString(rand.nextInt(31)+1));
+        bt2_6.setText(Integer.toString(rand.nextInt(31)+1));
+        bt3_6.setText(Integer.toString(rand.nextInt(31)+1));
+        bt4_6.setText(Integer.toString(rand.nextInt(31)+1));
+        bt5_6.setText(Integer.toString(rand.nextInt(31)+1));
+
         switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (switch1.isChecked())
                 {
+                    podramka.setVisibility(View.VISIBLE);
                     pod16.setVisibility(View.VISIBLE);
                     pod8.setVisibility(View.VISIBLE);
                     pod4.setVisibility(View.VISIBLE);
@@ -73,6 +100,7 @@ public class Level extends AppCompatActivity implements View.OnClickListener {
                 }
                 else
                 {
+                    podramka.setVisibility(View.GONE);
                     pod16.setVisibility(View.GONE);
                     pod8.setVisibility(View.GONE);
                     pod4.setVisibility(View.GONE);
@@ -130,6 +158,7 @@ public class Level extends AppCompatActivity implements View.OnClickListener {
         String currentText4 = bt1_4.getText().toString();
         String currentText5 = bt1_5.getText().toString();
         String currentText6 = bt1_6.getText().toString();
+     //   bt1_6.setText(random);
         if (v.getId() == R.id.bt1_1)
             bt1_1.setText(currentText.equals("0") ? "1" : "0" );
         if (v.getId() == R.id.bt1_2)
@@ -155,10 +184,7 @@ public class Level extends AppCompatActivity implements View.OnClickListener {
                 bt1_5.setVisibility(View.INVISIBLE);
                 bt1_6.setVisibility(View.INVISIBLE);
             }
-        if (switch1.isChecked())
-        {
-            pod16.setText("0");
-        }
+
 
 
 
